753692e4eb16f9b0b2a936008a7cd0b44cd55f80
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-07-02 08:19:04 -0400

    Add win verification

--- script.js
+++ script.js
@@ -35,8 +35,77 @@ function gameBoard() {
     return row < rows && row >= 0 && col < cols && col >= 0;
   };
   const getBoard = () => board;
+  const checkWin = () => {
+    for (let i = 0; i < rows; i++) {
+      const firstCell = board[i][0];
+      let equalCells = 1;
+      for (let j = 1; j < cols; j++) {
+        if (
+          !firstCell.isFree() &&
+          firstCell.getMarker() === board[i][j].getMarker()
+        ) {
+          equalCells++;
+        } else {
+          break;
+        }
+      }
+      if (equalCells >= rows) {
+        return { cell: firstCell, index: i, type: "row" };
+      }
+    }
+    for (let i = 0; i < cols; i++) {
+      const firstCell = board[0][i];
+      let equalCells = 1;
+      for (let j = 1; j < rows; j++) {
+        if (
+          !firstCell.isFree() &&
+          firstCell.getMarker() === board[j][i].getMarker()
+        ) {
+          equalCells++;
+        } else {
+          break;
+        }
+      }
+      if (equalCells >= cols) {
+        return { cell: firstCell, index: i, type: "col" };
+      }
+    }
+    for (let i = 0; i < rows; i += 2) {
+      let firstCell = board[0][0];
+      let equalCells = 1;
+      for (let j = 1; j < rows; j++) {
+        if (
+          !firstCell.isFree() &&
+          firstCell.getMarker() === board[j][j].getMarker()
+        ) {
+          equalCells++;
+        } else {
+          break;
+        }
+      }
+      if (equalCells >= cols) {
+        return { cell: firstCell, index: i, type: "diag" };
+      }
+      firstCell = board[0][cols - 1];
+      equalCells = 1;
+      for (let j = 1; j < cols; j++) {
+        if (
+          !firstCell.isFree() &&
+          firstCell.getMarker() === board[j][cols - j - 1].getMarker()
+        ) {
+          equalCells++;
+        } else {
+          break;
+        }
+      }
+      if (equalCells >= cols) {
+        return { cell: firstCell, index: i, type: "diag" };
+      }
+    }
+    return { cell: null, index: -1, type: "none" };
+  };
   newBoard();
-  return { getBoard, newBoard, printBoard, playMove };
+  return { getBoard, newBoard, printBoard, playMove, checkWin };
 }
 
 function cell() {
@@ -58,17 +127,31 @@ function gameController() {
   const playerOne = createPlayer("player1", "O");
   const playerTwo = createPlayer("player2", "X");
   const player = [playerOne, playerTwo];
-  let currentPlayer = playerOne;
   const game = gameBoard();
+  let currentPlayer = playerOne;
+  let finished = 0;
   const playTurn = (row, col) => {
+    if (finished) {
+      console.log("Game has already finished");
+      return;
+    }
     console.log(
       `${currentPlayer.getName()} makes a move in: (${row}, ${col}) (row, column)`
     );
     if (game.playMove(row, col, currentPlayer)) {
       game.printBoard();
+      checkWinner();
       switchPlayer(currentPlayer);
     } else {
       console.log("Invalid move, choose another cell");
+    }
+  };
+  const checkWinner = () => {
+    const checkWin = game.checkWin();
+    if (checkWin.type !== "none") {
+      console.log(checkWin);
+      console.log(`player with marker ${checkWin.cell.getMarker()} has won`);
+      finished = 1;
     }
   };
   const switchPlayer = (current) => {
@@ -78,3 +161,12 @@ function gameController() {
   return { game, playTurn };
 }
 const board = gameController();
+board.playTurn(2, 0);
+board.playTurn(0, 0);
+board.playTurn(1, 1);
+board.playTurn(0, 1);
+board.playTurn(0, 2);
+// board.playTurn(2, 1);
+// board.playTurn(2, 0);
+// board.playTurn(0, 2);
+// board.playTurn(2, 2);
